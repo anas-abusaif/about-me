f73a7ab7673f9a9f910f339466dd27b89f3e7fb2
anas-abusaif <anasabusief@gmail.com>
2021-06-08 16:33:48 +0300

    added lab-03 requirments

--- JS/app.js
+++ JS/app.js
@@ -1,55 +1,97 @@
 'use strict';
 
-let userName=prompt('please enter your name');
-document.getElementById('user').innerHTML='hello ' +userName+ '!, welcome to my website.';
+let userScore = 0
 
-let vacation=prompt('would i like to go on a vacation any time soon?').toLowerCase();
-if(vacation==='yes' || vacation==='y'){
+let userName = prompt('please enter your name');
+document.getElementById('user').innerHTML = 'hello ' + userName + '!, welcome to my website.';
+
+
+let vacation = prompt('would i like to go on a vacation any time soon?').toLowerCase();
+if (vacation === 'yes' || vacation === 'y') {
   //console.log('you are correct')
   alert('you are correct');
-} else if(vacation==='no' || vacation==='n'){
+  userScore++
+} else if (vacation === 'no' || vacation === 'n') {
   // console.log('you are mistaken :)')
 } else {
   alert('please enter (yes) or (y) or (no) (n)!');
 }
 
-let hunger=prompt('am i hungry?').toLowerCase();
-if(hunger==='yes' || hunger==='y'){
+let bachDegr = prompt("am i a bachelor's degree holder? ").toLowerCase();
+if (bachDegr === 'yes' || bachDegr === 'y') {
   //console.log('you are correct')
   alert('you are correct');
-} else if(hunger==='no' || hunger==='n'){
+  userScore++
+} else if (bachDegr === 'no' || bachDegr === 'n') {
   // console.log('you are mistaken :)')
 } else {
   alert('please enter (yes) or (y) or (no) (n)!');
 }
-let thirst=prompt('am i thirsty?').toLowerCase();
-if(thirst==='yes' || thirst==='y'){
-//console.log('you are correct')
-  alert('you are correct');
-} else if(thirst==='no' || thirst==='n'){
-// console.log('you are mistaken :)')
+let residence = prompt('do i live in london?').toLowerCase();
+if (residence === 'yes' || residence === 'y') {
+  //console.log('you are mistaken')
+  alert('you are mistaken');
+} else if (residence === 'no' || residence === 'n') {
+  // console.log('you are correct :)')
+  alert('you are correct')
+  userScore++
 } else {
   alert('please enter (yes) or (y) or (no) (n)!');
 }
 
-let mangoSmoothie=prompt('would i like a glass of cold mango smoothie?').toLowerCase();
-if(mangoSmoothie==='yes' || mangoSmoothie==='y')
-{
-//console.log('you are correct')
+let mangoSmoothie = prompt("do i keep unnecessarily bying games i'll never play on every steam sales season?").toLowerCase();
+if (mangoSmoothie === 'yes' || mangoSmoothie === 'y') {
+  //console.log('you are correct')
   alert('you are correct');
-} else if(mangoSmoothie==='no' || mangoSmoothie==='n')
-{
-// console.log('you are mistaken :)')
+  userScore++
+} else if (mangoSmoothie === 'no' || mangoSmoothie === 'n') {
+  // console.log('you are mistaken :)')
 } else {
   alert('please enter (yes) or (y) or (no) (n)!');
 }
-let nightRide=prompt('do i like going on night rides?').toLowerCase();
-if(nightRide==='yes' || nightRide==='y'){
-//console.log('you are correct')
+let nightRide = prompt('do i like going on night rides?').toLowerCase();
+if (nightRide === 'yes' || nightRide === 'y') {
+  //console.log('you are correct')
   alert('you are correct');
-} else if(nightRide==='no' || nightRide==='n'){
+  userScore++
+} else if (nightRide === 'no' || nightRide === 'n') {
   // console.log('you are mistaken :)')
 } else {
   alert('please enter (yes) or (y) or (no) (n)!');
 }
 
+var myHeight = prompt('guess how tall i am in centimeter!');
+let x = 0;
+
+while ( x < 3) {
+  if (myHeight == 180) {
+    alert('correct');
+    userScore++;
+    break;
+  } else if (myHeight > 180) {
+    var myHeight = prompt('too high!, try again');
+    x++;
+    if (x=3){alert('i am 180 centemeters high');}
+
+  } else if (myHeight < 180) {
+    var myHeight = prompt('too low! try again')
+    x++;
+    if (x=3){alert('i am 180 centemeters high');}
+
+  } else {
+    var myHeight = prompt('please enter a number');
+  }
+}
+
+let favFruits=['mango', 'strawberry', 'orange','cherry','melon']
+var ff=prompt('guess one of my favorite fruits!')
+for(let f=0; f<6; f++){
+  if (ff==favFruits[0] || ff==favFruits[1] || ff==favFruits[2] || ff==favFruits[3] || ff==favFruits[4]){
+    alert('you are correct!');
+    userScore++
+    break
+  }else{var ff=prompt('incorrect!, please try again');
+}
+}
+
+document.getElementById('score').innerHTML ='your final score is ' +userScore+ ' out of 7'
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # about-me
+
+A website to intreduce myself to the user
+
+Tuesday, june 8 2021
+
+added multiple prompts and loops in (app.js)
