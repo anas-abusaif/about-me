182b6a59114aae90dd75884ff9303ad68184c3fa
anas-abusaif <anasabusief@gmail.com>
2021-06-12 14:12:33 +0300

    added css proprities

--- JS/app.js
+++ JS/app.js
@@ -6,68 +6,14 @@ let userName = prompt('please enter your name');
 document.getElementById('user').innerHTML = 'hello ' + userName + '!, welcome to my website.';
 
 
-// let vacation = prompt('would i like to go on a vacation any time soon?').toLowerCase();
-// if (vacation === 'yes' || vacation === 'y') {
-//   //console.log('you are correct')
-//   alert('you are correct');
-//   userScore++;
-// } else if (vacation === 'no' || vacation === 'n') {
-//   // console.log('you are mistaken :)')
-// } else {
-//   alert('please enter (yes) or (y) or (no) (n)!');
-// }
-
-// let bachDegr = prompt("am i a bachelor's degree holder?").toLowerCase();
-// if (bachDegr === 'yes' || bachDegr === 'y') {
-//   //console.log('you are correct')
-//   alert('you are correct');
-//   userScore++;
-// } else if (bachDegr === 'no' || bachDegr === 'n') {
-//   // console.log('you are mistaken :)')
-// } else {
-//   alert('please enter (yes) or (y) or (no) (n)!');
-// }
-// let residence = prompt('do i live in london?').toLowerCase();
-// if (residence === 'yes' || residence === 'y') {
-//   //console.log('you are mistaken')
-//   alert('you are mistaken');
-// } else if (residence === 'no' || residence === 'n') {
-//   // console.log('you are correct :)')
-//   alert('you are correct');
-//   userScore++;
-// } else {
-//   alert('please enter (yes) or (y) or (no) (n)!');
-// }
-
-// let mangoSmoothie = prompt("do i keep unnecessarily bying games i'll never play on every steam sales season?").toLowerCase();
-// if (mangoSmoothie === 'yes' || mangoSmoothie === 'y') {
-//   //console.log('you are correct')
-//   alert('you are correct');
-//   userScore++;
-// } else if (mangoSmoothie === 'no' || mangoSmoothie === 'n') {
-//   // console.log('you are mistaken :)')
-// } else {
-//   alert('please enter (yes) or (y) or (no) (n)!');
-// }
-// let nightRide = prompt('do i like going on night rides?').toLowerCase();
-// if (nightRide === 'yes' || nightRide === 'y') {
-//   //console.log('you are correct')
-//   alert('you are correct');
-//   userScore++;
-// } else if (nightRide === 'no' || nightRide === 'n') {
-//   // console.log('you are mistaken :)')
-// } else {
-//   alert('please enter (yes) or (y) or (no) (n)!');
-// }
-
 function guissingGame (userInput)
 {
-  if (userInput=='y'|| userInput=='y'){
+  if (userInput==='y'|| userInput==='y'){
     alert('you are correct :)');
     userScore++;
-  }else if (userInput=='no' || userInput=='n'){
-    alert('you are mistaken :(')
-  }else {alert('please enter (yes) or (y) or (no) or (n)')}
+  }else if (userInput==='no' || userInput==='n'){
+    alert('you are mistaken :(');
+  }else {alert('please enter (yes) or (y) or (no) or (n)');}
 }
 let vacation=prompt('would i like to go on a vacation any time soon?').toLocaleLowerCase();
 guissingGame(vacation);
@@ -75,14 +21,24 @@ guissingGame(vacation);
 let bachDegr =prompt("m i a bachelor's degree holder?").toLocaleLowerCase();
 guissingGame(bachDegr);
 
-let residence=prompt('do i live in london?').toLocaleLowerCase();
-guissingGame(residence);
-
 let mangoSmoothie=prompt("do i keep unnecessarily bying games i'll never play on every steam sales season?").toLocaleLowerCase();
 guissingGame(mangoSmoothie);
 
 let nightRide=prompt('do i like going on night rides?').toLocaleLowerCase();
 guissingGame(nightRide);
+
+function guissingGameWrong (userInput2)
+{
+  if (userInput2==='y'|| userInput2==='y'){
+    alert('you are mistakent :(');
+  }else if (userInput2==='no' || userInput2==='n'){
+    alert('you are correct :)');
+    userScore++;
+  }else {alert('please enter (yes) or (y) or (no) or (n)');}
+}
+let residence=prompt('do i live in london?').toLocaleLowerCase();
+guissingGameWrong(residence);
+
 
 
 
@@ -110,7 +66,7 @@ while ( x < 3) {
   }
 }
 
-let favFruits=['mango', 'strawberry', 'orange','cherry','melon']
+let favFruits=['mango', 'strawberry', 'orange','cherry','melon'];
 var ff=prompt('guess one of my favorite fruits!')
 for(let f=0; f<6; f++){
   if (ff===favFruits[0] || ff===favFruits[1] || ff===favFruits[2] || ff===favFruits[3] || ff===favFruits[4]){
